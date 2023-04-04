8662dcde52b250ecc1e2bcad8e3ce4b0fcd57c9b
Phong Nguyen <xuanphong09t1@gmail.com>
2023-04-04 23:37:27 +0900

    add sam

--- sam/commands.sh
+++ sam/commands.sh
@@ -1,9 +1,9 @@
 # create an s3 bucket
-aws s3 mb s3://stephane-code-sam
+aws s3 mb s3://phongnx-code-sam
 
 # package cloudformation
-aws cloudformation package  --s3-bucket stephane-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+aws cloudformation package  --s3-bucket phongnx-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
 # sam package ... 
 
 # deploy 
-aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name phongnx-hello-world-sam --capabilities CAPABILITY_IAM
